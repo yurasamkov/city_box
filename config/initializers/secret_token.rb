359be6c051765cacc83a11f50f712fe278c3d95b
Yura Samkov <y.samkoff@ukr.net>
2015-07-27 20:10:44 +0300

    (PagesController) (bootstrap-sass)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CityBox::Application.config.secret_key_base = '2261f3d67f445af4a97d6acca95daacca69e14f9c7882a768fcfbe78ab5c802f46fb280ab6d9a7cc23f513476e91b5db926e2935b2bad7fb583e9acdf52ece18'
