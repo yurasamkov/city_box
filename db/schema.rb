84e6e9dfdfaac5123519b2b314d78f130deed344
Yura Samkov <y.samkoff@ukr.net>
2015-07-29 14:59:11 +0300

    post, button

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150728195217) do
+ActiveRecord::Schema.define(version: 20150729092609) do
 
   create_table "posts", force: true do |t|
-    t.string   "title"
-    t.string   "text"
+    t.string   "content"
+    t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
