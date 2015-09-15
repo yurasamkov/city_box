7ff22e3b157ee6e7cd8995cc2d397b6307b88d91
Yura Samkov <y.samkoff@ukr.net>
2015-09-15 14:00:01 +0300

    add validates to Comments, validates: body, length

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ActiveRecord::Base
 
   #attr_accessible :body, :commenter, :post
+  validates :body, presence: true, length: { maximum: 100 }
   belongs_to :post
   belongs_to :user
 
